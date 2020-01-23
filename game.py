ea9af5a93c3301806318ebdf4310834e454df142
Nathan Merrill <mathiscool3000@gmail.com>
2020-01-23 20:15:11 +0000

    Add player movement

--- game.py
+++ game.py
@@ -3,12 +3,15 @@ from graphics import *
 makeGraphicsWindow(1000, 800)
 
 def startWorld(world):
-    pass
+    world.playerX = 500
 
 def updateWorld(world):
-    pass
+    if isKeyPressed('d'):
+        world.playerX += 5
+    if isKeyPressed('a'):
+        world.playerX -= 5
 
 def drawWorld(world):
-    pass
+    fillCircle(world.playerX, 720, 30, "dimgrey")
 
 runGraphics(startWorld, updateWorld, drawWorld)
