ad3ffc010933d2e8ebcfe06ee60c6bc3733baab7
Nathan Merrill <mathiscool3000@gmail.com>
2020-01-23 20:10:16 +0000

    Add basic graphics boilerplate

---
+++ game.py
@@ -0,0 +1,14 @@
+from graphics import *
+
+makeGraphicsWindow(1000, 800)
+
+def startWorld(world):
+    pass
+
+def updateWorld(world):
+    pass
+
+def drawWorld(world):
+    pass
+
+runGraphics(startWorld, updateWorld, drawWorld)
